ac9b330d903a4e5ec378de24f31137f24b2b2632
talhaahmed884 <talhaahmed884@gmail.com>
2025-10-06 22:45:05 -0700

    1. added more customers in the driver code

--- src/com/cpp/homeWork3/factory/Driver.java
+++ src/com/cpp/homeWork3/factory/Driver.java
@@ -2,19 +2,22 @@ package com.cpp.homeWork3.factory;
 
 public class Driver {
     public static void main(String[] args) {
-        Customer customer1 = new Customer("Alice", NoRestrictionDietPlan.getInstance());
+        Customer customer1 = new Customer("Mark", NoRestrictionDietPlan.getInstance());
         System.out.println(customer1);
 
-        Customer customer2 = new Customer("Alice", NoRestrictionDietPlan.getInstance());
+        Customer customer2 = new Customer("Robert", NoRestrictionDietPlan.getInstance());
         System.out.println(customer2);
 
-        Customer customer3 = new Customer("Alice", NutAllergyDietPlan.getInstance());
+        Customer customer3 = new Customer("Williams", NutAllergyDietPlan.getInstance());
         System.out.println(customer3);
 
-        Customer customer4 = new Customer("Alice", PaleoDietPlan.getInstance());
+        Customer customer4 = new Customer("Sophie", PaleoDietPlan.getInstance());
         System.out.println(customer4);
 
-        Customer customer5 = new Customer("Alice", VeganDietPlan.getInstance());
+        Customer customer5 = new Customer("Sohan", VeganDietPlan.getInstance());
         System.out.println(customer5);
+
+        Customer customer6 = new Customer("Tom", PaleoDietPlan.getInstance());
+        System.out.println(customer6);
     }
 }
